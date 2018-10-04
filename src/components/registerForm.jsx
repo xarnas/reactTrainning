18c6337b9f453ad3a8737be239ec52bafcfe4675
xarnas <arnpetrauskas@gmail.com>
2018-10-05 00:03:22 +0300

    registerForm

--- src/components/registerForm.jsx
+++ src/components/registerForm.jsx
@@ -1,34 +1,41 @@
-import React, { Component } from "react";
+import React from "react";
 import Joi from "joi-browser";
 import Form from "../common/form";
-class LoginForm extends Form {
+
+class RegisterForm extends Form {
   state = {
-    data: { username: "", password: "" },
+    data: { username: "", password: "", name: "" },
     errors: {}
   };
   schema = {
     username: Joi.string()
       .required()
+      .email()
       .label("Username"),
     password: Joi.string()
       .required()
-      .label("Password")
+      .min(5)
+      .label("Password"),
+    name: Joi.string()
+      .required()
+      .label("Name")
   };
-  //username = React.createRef();
   doSubmit = () => {
     console.log("Submitted");
   };
   render() {
     return (
       <div>
-        <h1>Login Form</h1>
+        <h1>Register</h1>
         <form onSubmit={this.handleSubmit}>
           {this.renderInput("username", "Username")}
           {this.renderInput("password", "Password", "password")}
-          {this.renderButton("Login")}
+          {this.renderInput("name", "Name")}
+          {this.renderButton("Register")}
         </form>
       </div>
     );
   }
 }
-export default LoginForm;
+
+export default RegisterForm;
