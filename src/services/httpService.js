775c19797c4a63eacc83346acd22afac5c3d8076
xarnas <arnpetrauskas@gmail.com>
2018-11-11 23:49:11 +0200

    http request

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -4,7 +4,9 @@ import { toast } from "react-toastify";
 
 axios.interceptors.response.use(null, error => {
   const expectedError =
-    error.response && error.reponse.status >= 400 && error.reponse.status < 500;
+    error.response &&
+    error.response.status >= 400 &&
+    error.response.status < 500;
   if (!expectedError) {
     logger.log(error);
     toast.error("An unexpected error occurrred");
